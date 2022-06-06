3ac8b21c60a97ff59d4af83098bab22e493f6b6a
leedakoog <leedakoog@naver.com>
2022-06-06 22:52:01 +0900

    public 추가

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ const home = require("./src/routes/home")
 
 app.set("views", "./src/views")
 app.set("view engine", "ejs")
+app.use(express.static(`${__dirname}/src/public`)) //정적 경로 추가, 디렉터리네임은 현재 파일이 있는 경로를 반환
 
 app.use("/", home)  // use -> 미들 웨어를 등록해주는 메서드
 
